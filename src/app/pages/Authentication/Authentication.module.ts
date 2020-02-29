f49eeec49efc041296e3f0cc6faf94259090df5f
Juan Daniel Ozuna Espinal <juandanielozuna2@gmail.com>
2020-02-28 21:00:41 -0400

    Created first route

---
+++ src/app/pages/Authentication/Authentication.module.ts
@@ -0,0 +1,35 @@
+import { LoginPageComponent } from './LoginPage/LoginPage.component';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationLayoutComponent } from './authentication-layout/authentication-layout.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {
+    path: '',
+    component: AuthenticationLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginPageComponent
+      }
+    ]
+  }
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [
+    AuthenticationLayoutComponent
+  ]
+})
+export class AuthenticationModule { }
